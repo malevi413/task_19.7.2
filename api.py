d552062fd482136abe865bc3ba9cbe173498f72e
Max Levin <110128097+malevi413@users.noreply.github.com>
2022-09-11 21:14:08 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,12 +1,9 @@
-"""Модуль 19"""
 import json
-
 import requests
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 
 class PetFriends:
-    """апи библиотека к веб приложению Pet Friends"""
 
     def __init__(self):
         self.base_url = "https://petfriends.skillfactory.ru/"
